4efab45e204330884622f862400d7bbf3d302398
mayuri <mayu.reddy10@gmail.com>
2026-03-23 16:03:38 +1100

    terraform

--- variables.tf
+++ variables.tf
@@ -10,10 +10,26 @@ variable "domain_name" {
     default = "daws-88s.online"
 }
 
+variable "component" {
+    type = "string"
+}
+
 variable "zone_id" {
     default = "Z0600999D8XNX0JXQIHH"
 }
 
+variable "health_check_path" {
+    default = "/health"
+}
+
 variable "app_version" {
     default = "v3"
+}
+
+variable "port_no" {
+    default = 8080
+}
+
+variable "rule_priority" {
+    type = number
 }
